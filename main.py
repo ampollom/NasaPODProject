999c2ee38d779ca0daff0bc2d00e56ae364a6baa
Amy <amy.pollom@gmail.com>
2023-12-29 19:04:57 -0800

    organized code into methods

--- main.py
+++ main.py
@@ -9,28 +9,33 @@ import os
 
 #Retrieves html from NASA website
 URL = "https://apod.nasa.gov/apod/"
-page = requests.get(URL)
 current_datetime = datetime.now().strftime("(%Y.%m.%d)")
+links = []
 
+def parseUrl(URL):
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, "html.parser")
+    retriveIMGTag(soup, links)
 #print(page.text)
 
 #Parses HTML and locates image tags
-soup = BeautifulSoup(page.content, "html.parser")
-image_tags = soup.find_all('img')
-links = []
-for image_tag in image_tags:
-    links.append("https://apod.nasa.gov/apod/" + image_tag['src'])
-
+def retriveIMGTag(soup, links):
+    image_tags = soup.find_all('img')
+    for image_tag in image_tags:
+        links.append("https://apod.nasa.gov/apod/" + image_tag['src'])
+    retreiveIMGFile(links)
 #print(links)
 
 #Collects and saves image with current date from link
-urllib.request.urlretrieve(links[0], 'images/nasapictureoftheday'+ current_datetime +'.png')
+def retreiveIMGFile(links):
+    urllib.request.urlretrieve(links[0], 'images/nasapictureoftheday'+ current_datetime +'.png')
 
+def displayImage():
 #Opens image from saved file
-dimensions = (1926, 2000)
-i = Image.open('images/nasapictureoftheday'+ current_datetime +'.png')
-i.thumbnail(dimensions)
-i.show()
+    dimensions = (1926, 2000)
+    i = Image.open('images/nasapictureoftheday'+ current_datetime +'.png')
+    i.thumbnail(dimensions)
+    i.show()
 
 #use beautiful soup to get dimensions
 #investigate setting up Azure as storage
@@ -38,3 +43,7 @@ i.show()
 #clean up code into methods
 #figure out how to include explanation with image
 
+#send code to Github
+
+parseUrl(URL)
+displayImage()
