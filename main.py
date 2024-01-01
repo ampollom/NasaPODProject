3a139d8a0c83f9ce273816fadfd420306cad9f14
Amy <amy.pollom@gmail.com>
2023-12-31 20:46:03 -0800

    updated code to handle video links

--- main.py
+++ main.py
@@ -13,14 +13,39 @@ current_datetime = datetime.now().strftime("(%Y.%m.%d)")
 links = []
 
 def parseUrl(URL):
+    #requests and parses the page from the provided url
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
-    retriveIMGTag(soup, links)
+
+    #checks for image and video tags
+    image_tags = soup.find_all('img')
+    video_tags = soup.find_all('iframe')
+
+    #sends links to the appropriate method
+    if len(video_tags) != 0:
+        retriveVideoTags(video_tags, links)
+    elif len(image_tags) != 0:
+        retriveIMGTag(image_tags, links)
+    else:
+        print("Houston we have a problem")
+
+
+    #print(soup)
+
+    #retriveIMGTag(soup, links)
 #print(page.text)
 
+#prints the url for the video
+def retriveVideoTags(video_tags, links):
+    #video_tags = soup.find_all('iframe')
+    for video_tag in video_tags:
+        links.append(video_tag['src'])
+        print("No image today, check out this video: " + links[0])
+
+
 #Parses HTML and locates image tags
-def retriveIMGTag(soup, links):
-    image_tags = soup.find_all('img')
+def retriveIMGTag(image_tags, links):
+    #image_tags = soup.find_all('img')
     for image_tag in image_tags:
         links.append("https://apod.nasa.gov/apod/" + image_tag['src'])
     retreiveIMGFile(links)
@@ -42,8 +67,8 @@ def displayImage():
 #investigate deleting images
 #clean up code into methods
 #figure out how to include explanation with image
-
+#errors: what if the image is a video?
 #send code to Github
 
 parseUrl(URL)
-displayImage()
+#displayImage()
